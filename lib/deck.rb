b333c039e3b6588f473c4bc9fc6f68a8e0b1cfa3
Julia Marco <marcootj@gmail.com>
2018-11-28 14:46:36 -0700

    adds changes

---
+++ lib/deck.rb
@@ -0,0 +1,25 @@
+require '../lib/card'
+require 'pry'
+
+
+class Deck
+attr_reader :cards, :count
+
+  def initialize(cards)
+    @cards = cards
+    @count = @cards.length
+    @cards_ordered_by_category = []
+  end
+
+
+
+  def cards_in_category(category)
+    cards.each do |card|
+      if card.category == category
+        @cards_ordered_by_category << card
+      end
+    end
+    @cards_ordered_by_category
+  end
+
+end
---
+++ test/deck_test.rb
@@ -0,0 +1,40 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require '../lib/card'
+require '../lib/deck'
+require 'pry'
+
+class DeckTest < Minitest::Test
+
+  def test_deck_exists
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_instance_of Deck, deck
+  end
+
+  def test_number_of_cards
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal 3, deck.count
+  end
+
+  def test_cards_in_a_specific_category
+
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal cards[1, 2], deck.cards_in_category(:STEM)
+  end
+
+end
