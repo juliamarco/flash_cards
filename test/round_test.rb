d3501b4cb5b11965b67d2cee5a3803d3832fc91f
Julia Marco <marcootj@gmail.com>
2018-11-28 15:22:19 -0700

    Adds new method

--- test/round_test.rb
+++ test/round_test.rb
@@ -23,7 +23,17 @@ class RoundTest < Minitest::Test
     card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
     deck = Deck.new([card_1, card_2, card_3])
     round = Round.new(deck)
+
     assert_instance_of Deck, round.deck
   end
 
+  def test_turns_method
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    deck = Deck.new([card_1, card_2, card_3])
+    round = Round.new(deck)
+
+    assert_equal [], round.turns
+  end
 end
