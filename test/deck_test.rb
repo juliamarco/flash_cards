a838cff95a09c840935429ece7eb2feb83008412
Julia Marco <marcootj@gmail.com>
2018-11-28 14:53:40 -0700

    Adds new method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -47,4 +47,13 @@ class DeckTest < Minitest::Test
     assert_equal [cards[0]], deck.cards_in_category(:Geography)
   end
 
+  def test_card_in_a_nonexistent_category
+    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    cards = [card_1, card_2, card_3]
+    deck = Deck.new(cards)
+
+    assert_equal [], deck.cards_in_category("Pop Culture")
+  end
 end
