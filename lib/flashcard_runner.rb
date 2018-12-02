fb7178bdc8871625f849491b5744c77cbcd9bffb
juliamarco <35079289+juliamarco@users.noreply.github.com>
2018-12-02 08:40:31 -0700

    Add files via upload
    Updated changes

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -1,16 +1,13 @@
+require './lib/card_generator'
 require './lib/card'
 require './lib/deck'
 require './lib/round'
 require './lib/turn'
 require 'pry'
 
+filename = "cards.txt"
 
-
-
-card_1 = Card.new("Who painted a late 15th-century mural known as the Last Supper?", "LEONARDO DA VINCI", :Art)
-card_2 = Card.new("What is the meaning of the French expression 'C'est la vie'?", "THAT'S LIFE", :Language)
-card_3 = Card.new("Penicillin was discovered in 1928 by which Scottish scientist?", "ALEXANDER FLEMING", :Science)
-card_4 = Card.new("What is the word for 'Hello' in Spanish?", "HOLA", :Language)
-deck = Deck.new([card_1, card_2, card_3, card_4])
+cards = CardGenerator.new(filename).create_deck
+deck = Deck.new(cards)
 round = Round.new(deck)
 round.start
